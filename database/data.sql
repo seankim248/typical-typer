d38129706b8a2a493bd1d26391ad62013e12d3c9
Sean Kim <seankim2480@gmail.com>
2022-02-14 23:41:04 +0000

    updated files to properly add data to database

--- database/data.sql
+++ database/data.sql
@@ -1,3 +1,3 @@
 insert into "quotes" ("quoteId", "content")
 values ('1', 'At the end of the day, you are solely responsible for your success and your failure. And the sooner you realize that, you accept that, and integrate that into your work ethic,
-you will start being successful.As long as you blame others for the reason you aren\'t where you want to be, you will always be a failure.')
+you will start being successful.As long as you blame others for the reason you aren''t where you want to be, you will always be a failure.')
