5971e4104ed690e2b1a887298065c9f2d609d784
Sean Kim <seankim2480@gmail.com>
2022-02-28 02:56:16 +0000

    changed color scheme

--- client/components/prompt.jsx
+++ client/components/prompt.jsx
@@ -108,7 +108,7 @@ export default class Prompt extends React.Component {
           <div className='prompt' onClick={this.props.onClick} onKeyDown={this.handleKeyDown} tabIndex='0'>
               {
                 charList.map((char, index) => (
-                  <span key={char + index} id={this.handleBlinker(index)} className={char === ' ' ? 'space' : this.getCharClass(index)}>{char}</span>
+                  <span key={char + index} id={this.handleBlinker(index)} className={this.getCharClass(index)}>{char}</span>
                 ))
               }
           </div>
